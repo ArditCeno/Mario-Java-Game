cfb019fa36c04dd2e8208ba2a4c5f35a5d242e50
Ardit Ceno <arditceno1@gmail.com>
2026-04-20 23:07:14 +0200

    Setup JFrame and Game Window structure

--- MarioGame.java
+++ MarioGame.java
@@ -1,1 +1,20 @@
+import javax.swing.*;
 
+public class MarioGame extends JFrame{
+  public MarioGame(){
+    setTitle("Mario Game");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(false);
+
+  GamePanel gamePanel = new GamePanel();
+    add(gamePanel);
+
+  pack();
+    setLocationRelativeTo(null);
+  }
+  
+  public static void main (String []args ){
+SwingUtilities.invokeLater(() -> {
+            new MarioGame().setVisible(true);}
+                           }
+}
